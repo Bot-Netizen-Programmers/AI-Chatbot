b6fd8745275a4f2f26d9a9c7d5a67292fc05502d
Bot-Netizen-Programmers <83690529+Bot-Netizen-Programmers@users.noreply.github.com>
2023-07-28 17:00:31 +0000

    discord connection

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+#Application-ID : 1134525900986261515
+#Public-Key : 0c4738293c02edd3bf723c48a34c8842355bad712dc8d5946b9c115f8d0863d1
+
 import openai
+# This example requires the 'message_content' intent.
+import os
 import discord
+
+TOKEN = os.environ['SECRET_KEY']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('TOKEN')
 
